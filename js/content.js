fc13d76180056c25f50a8b1c91034ec104615ec8
Sangsoo Nam <sangsoo@spotify.com>
2021-09-06 21:37:12 +0200

    Remove unused code

--- js/content.js
+++ js/content.js
@@ -7,16 +7,12 @@ function replaceLink(pattern, url) {
     let re = new RegExp('(.*?)(' + pattern + ')(.*?)', 'i');
     let text = title.text().replace(re,"$1<a target='_blank' href='" + targetUrl + "$2'>$2</a>$3")
     title.html(text);
-    clearInterval(replaceIntervalId);
   }
 }
 
 function init() {
   getValues(function (patternValue, urlValue) {
     replaceLink(patternValue, urlValue);
-    $('.issue-title-link').click(function() {
-      replaceIntervalId = setTimeout(replaceLink, 2000);
-    });
   });
 }
 
